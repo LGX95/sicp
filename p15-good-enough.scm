d8ee3938328546f0bb2eb8d15b085bac191834b4
LGX95 <ligengxin95@gmail.com>
2019-05-04 17:45:30 +0800

    Add, exercise 1.6

---
+++ p15-good-enough.scm
@@ -0,0 +1,4 @@
+;;; p15-good-enough.scm
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
---
+++ p15-improve.scm
@@ -0,0 +1,7 @@
+;;; p15-improve.scm
+
+(load "p15-average.scm")
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
